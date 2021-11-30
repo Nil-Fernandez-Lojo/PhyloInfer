c6a09ed7d5d654b333055f22644064e7c86108e5
Nil Fernandez Lojo <nilfernandezlojo@gmail.com>
2021-11-30 12:09:39 +0100

    added doc to sample.py

--- sample.py
+++ sample.py
@@ -2,6 +2,31 @@ import numpy as np
 from scipy.stats import multinomial
 
 class Sample():
+	"""
+	Class used to represent a minibulk sample and its assignation to a node. 
+
+	...
+
+	Attributes
+	----------
+	read_count: np.array
+		list containing the reads counts for each segment
+	p_read: np.array
+		probability that a read spans the corresponding segment
+	node: Node
+		Node to which the sample is assigned
+	config: dict
+		dictionary containing configuration parameters
+
+	Methods
+	-------
+	set_p_read:
+		updates the p_read by using the CN porfile of the corresponding node
+	generate_sample_from_CN:
+		generates a random read_count from p_read and n_reads_sample in the config file
+	get_log_likelihood:
+		returns the log likelihood of read_count
+	"""
 	def __init__(self,node,config,read_count = None):
 		self.config = config
 		self.node = node
